f06d94c651e0ba3fa51eb2ef98433b62bcdc3c83
Shawn Xiong <Shawn.Xiong@iii.com>
2017-06-09 17:01:45 +0100

    ms-1_initial implementation, doesn't work

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,22 @@
 package observatory
 
 import java.time.LocalDate
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import java.nio.file.Paths
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.rdd.RDD
+import scala.util.matching.Regex
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+  
+  val spark = SparkSession.builder().appName("capstone").config("spark.master", "local").getOrCreate()
+  
+  import spark.implicits._
 
   /**
     * @param year             Year number
@@ -14,7 +25,23 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
-    ???
+    val stationsDF = spark.sparkContext.textFile(stationsFile).map { line =>  
+      line.split(",").toList match {
+        case List(stn, wban, latitude, longetude) if(latitude != null && longetude != null) => 
+          Station(stn = Option(stn), wban = Option(wban), latitude = Option(latitude.toDouble), longetude = Option(longetude.toDouble))
+      }
+    }.toDF().persist()
+    
+    val temperatureDF = spark.sparkContext.textFile(temperaturesFile).map { line =>  
+      line.split(",").toList match {
+        case List(stn, wban, month, day, temperatureInFehranheit) => 
+          Temperature(stn = Option(stn), wban = Option(wban), month = month.toInt, day = day.toInt, temperature = toCelcius(Option(temperatureInFehranheit).getOrElse("9999.9").toDouble))
+      }
+    }.toDF().persist()
+    
+    val result = stationsDF.join(temperatureDF, stationsDF("stn") <=> temperatureDF("stn") && stationsDF("wban") <=> temperatureDF("wban"), "inner").collectAsList()
+    
+    Seq(null, null, null)
   }
 
   /**
@@ -24,5 +51,20 @@ object Extraction {
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Double)]): Iterable[(Location, Double)] = {
     ???
   }
+  
+  def toCelcius(fahrenheit: Double): Double = (fahrenheit - 32.0) * 5.0 / 9.0
+  
+  
+  def extractYearFromFileName(filename: String): Option[Integer] = {
+    val pattern = """/([0-9]{4}).csv""".r
+    pattern.findFirstMatchIn(filename) match {
+      case Some(m) => Some(m.group(1).toInt)
+      case _ => None
+    }
+  }
 
 }
+
+case class Station(stn:Option[String], wban:Option[String], latitude:Option[Double], longetude:Option[Double])
+case class Temperature(stn:Option[String], wban:Option[String], month:Integer, day:Integer, temperature:Double)
+case class stationTemperatureRow()
--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -6,6 +6,17 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class ExtractionTest extends FunSuite {
-
+  
+  test("toCelcius") {
+    assert(Extraction.toCelcius(32) === 0.0)
+    assert(BigDecimal(Extraction.toCelcius(100)).setScale(2, BigDecimal.RoundingMode.HALF_UP) === 37.78)
+  }
+  
+  test("extractYearFromFilename") {
+    assert(Extraction.extractYearFromFileName("/1975.csv").getOrElse(0) === 1975)
+    assert(Extraction.extractYearFromFileName("/C/User/shawn.xiong/1988.csv").getOrElse(0) === 1988)
+    assert(Extraction.extractYearFromFileName("/200123.csv").getOrElse(0) === 0)
+    assert(Extraction.extractYearFromFileName("/12.csv").getOrElse(0) === 0)
+  }
   
 }
